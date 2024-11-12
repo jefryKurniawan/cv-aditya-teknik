75286b492a6abb00eb97c579fb480b4390abe7e9
zuX1 <jefrykurniawan067@gmail.com>
2024-11-12 15:50:03 +0700

    configure domain next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    target: 'serverless',
+    publicRuntimeConfig: {
+      domain: 'adytia-tehnik.com',
+    },
+}
+  
+
 export default nextConfig;
