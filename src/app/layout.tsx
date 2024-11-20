bd15855da8b56aafe9a64f6d3b7085995a7ce9e0
zuX1 <jefrykurniawan067@gmail.com>
2024-11-21 00:16:36 +0700

    update url icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,8 +32,14 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
   return (
     <html lang="id">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" href="/assets/logo/nidi-slo.png" sizes="48x48" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+
+        {/* <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/assets/logo/nidi-slo.png" sizes="48x48" /> */}
         <meta name="email" content="buchorimakmur8@gmail.com" />
         <meta property="og:email" content="buchorimakmur8@gmail.com" />
         <meta property="og:phone_number" content="+6281456123367" />
